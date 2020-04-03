afe21309f82b194ab669546e55329ae8da4a9745
madonkey <dermot.s.hughes@gmail.com>
2020-04-03 16:52:54 +0100

    Added alt text to 404 image

--- src/pages/404.js
+++ src/pages/404.js
@@ -11,7 +11,7 @@ const NotFoundPage = () => (
                     <p>Huh. It look's like that page can't be found. You can <Link to="/">return home</Link> to start over.</p>
                     <p>Here's a cute picture to make it up to you.</p>
                 </section>
-                <img className="random-image" alt="" src="https://source.unsplash.com/640x480?cute-animals"></img>
+                <img className="random-image" alt="Random cute animal image" src="https://source.unsplash.com/640x480?cute-animals"></img>
             </article>
         </div>
     </Layout>
