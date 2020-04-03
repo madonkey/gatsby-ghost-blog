d89f59d3c0f3b94c4c15f87f444d7ac55fdc9d51
madonkey <dermot.s.hughes@gmail.com>
2020-04-03 16:49:03 +0100

    Added cute animal 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -8,8 +8,10 @@ const NotFoundPage = () => (
             <article className="content" style={{ textAlign: `center` }}>
                 <h1 className="content-title">Error 404</h1>
                 <section className="content-body">
-                    Huh. It look's like that page can't be found. You can <Link to="/">return home</Link> to start over
+                    <p>Huh. It look's like that page can't be found. You can <Link to="/">return home</Link> to start over.</p>
+                    <p>Here's a cute picture to make it up to you.</p>
                 </section>
+                <img className="random-image" alt="" src="https://source.unsplash.com/640x480?cute-animals"></img>
             </article>
         </div>
     </Layout>
