e32c377037ca0b922e7752bca269dbfe988c0d18
madonkey <dermot.s.hughes@gmail.com>
2020-06-24 22:29:47 +0100

    Added comment to cv script

--- src/utils/generate-pdf.js
+++ src/utils/generate-pdf.js
@@ -1,10 +1,11 @@
+// This generates a pdf file of the current CV with `npm run generate-cv`
+
 const puppeteer = require(`puppeteer`);
 
 (async () => {
     const browser = await puppeteer.launch();
     let page = await browser.newPage();
-    await page.goto(`http://localhost:9000/cvraw/`);
-    // page = page.$(`#cv`);
+    await page.goto(`http://localhost:9000/cvraw/`); // Generate from a prod build
     await page.pdf({
         path: `./public/dermot-cv.pdf`, // path (relative to CWD) to save the PDF to.
         format: `A4`,
