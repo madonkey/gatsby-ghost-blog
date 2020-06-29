7fb7bc2b0e430b9d16dc186c08ee35ed9f81303c
madonkey <dermot.s.hughes@gmail.com>
2020-06-29 12:04:57 +0100

    Remove offline plugin to prevent caching

--- gatsby-config.js
+++ gatsby-config.js
@@ -184,7 +184,7 @@ module.exports = {
         `gatsby-plugin-catch-links`,
         `gatsby-plugin-react-helmet`,
         `gatsby-plugin-force-trailing-slashes`,
-        `gatsby-plugin-offline`,
+        // `gatsby-plugin-offline`,
         `gatsby-plugin-sass`,
     ],
 };
